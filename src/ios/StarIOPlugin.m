b05942a302516ef7a675267ea99efa001bc6ad94
Saul <saul@justcloudit.co>
2016-11-10 15:06:05 +0000

    Test

--- src/ios/StarIOPlugin.m
+++ src/ios/StarIOPlugin.m
@@ -395,7 +395,7 @@ static NSString *dataCallbackId = nil;
     [dict setObject:[portInfo portName] forKey:@"portName"];
     [dict setObject:[portInfo macAddress] forKey:@"macAddress"];
     [dict setObject:[portInfo modelName] forKey:@"modelName"];
-    [dict setObject:[NSNumber numberWithBool:[portInfo isConnected]] forKey:@"isConnected"];
+    [dict setObject:[NSNumber numberWithBool:YES] forKey:@"isConnected"];
     return dict;
 }
 
